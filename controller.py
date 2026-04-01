28d5a39df0396a828a4918528698ebc9034ea90e
Javokhir Bakhritdinov <Javokhir.Bakhritdinov@usm.edu>
2026-04-01 11:59:53 -0500

    Add num_people_input and update tip calculation

--- controller.py
+++ controller.py
@@ -7,20 +7,24 @@ Builder.load_file('view.kv')
 
 class TipCalculator(BoxLayout):
     bill_input = StringProperty()
+    num_people_input = StringProperty()
     tip_output = StringProperty()
-    
+    split_output = StringProperty()
+
     __model: TipModel
 
     def __init__(self, model):
-        super().__init__()    
+        super().__init__()
         self.__model = model
 
     def btn_click_calc_tip(self, tip_percent):
         try:
             self.__model.bill_amount = float(self.bill_input)
             self.__model.tip_percent = float(tip_percent)
-            
+            self.__model.num_people = int(self.num_people_input)
+
             self.tip_output = f"Tip Amount: ${self.__model.tip_amount:.2f}"
+            self.split_output = f"Total Per Person: ${self.__model.total_per_person:.2f}"
 
         except ValueError as ex:
             print("error")
